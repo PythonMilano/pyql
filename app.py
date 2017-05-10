9f3a75f2958e86a447cddcf15530a1b496b5d77c
Christian Strappazzon <christian.strappazzon@indicod-ecr.it>
2017-05-10 14:44:43 +0200

    Missing property

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def get_results(response, index=0):
     return {
         'topic': obj.name,
         'day': date.day,
-        'month': calendar.month_name[date - 1].upper(),
+        'month': calendar.month_name[date.month - 1].upper(),
         'link': obj.link,
     }
 
