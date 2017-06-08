66eec6b5901250ec119b74a9f58ace861a8f76f6
Christian Strappazzon <cstrap@users.noreply.github.com>
2017-06-08 14:34:37 +0200

    JSON response meetups

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-# -*- coding: utf-8 -*-
+
 """
 Meetup API proxy.
 """
 
-import arrow
 import requests
+
+import arrow
 from box import Box
 from flask import Flask, jsonify
 from flask_cors import cross_origin
 
-
 app = Flask(__name__)
 
-MONTHS = ['', 'GENNAIO', 'FEBBRAIO', 'MARZO', 'APRILE', 'MAGGIO', 'GIUGNO', 'LUGLIO', 'AGOSTO', 'SETTEMBRE', 'OTTOBRE', 'NOVEMBRE', 'DICEMBRE']
+MONTHS = ['', 'GENNAIO', 'FEBBRAIO', 'MARZO', 'APRILE', 'MAGGIO', 'GIUGNO',
+          'LUGLIO', 'AGOSTO', 'SETTEMBRE', 'OTTOBRE', 'NOVEMBRE', 'DICEMBRE']
 
-CURRENT_EVENTS = 'https://api.meetup.com/Python-Milano/events'
-PAST_EVENTS = 'https://api.meetup.com/milano-scala-group/events/?status=past'
+CURRENT_EVENTS = 'https://api.meetup.com/{meetup}/events'
+PAST_EVENTS = 'https://api.meetup.com/{meetup}/events/?status=past'
 
 
 def get_results(response, index=0):
@@ -31,13 +32,13 @@ def get_results(response, index=0):
     }
 
 
-def return_value():
+def return_value(meetup='Python-Milano'):
     """ meetup API call """
-    response = requests.get(CURRENT_EVENTS)
+    response = requests.get(CURRENT_EVENTS.format(meetup=meetup))
     if response.status_code == 200 and response.json():
         return get_results(response)
 
-    response = requests.get(PAST_EVENTS)
+    response = requests.get(PAST_EVENTS.format(meetup=meetup))
     if response.status_code == 200 and response.json():
         return get_results(response, -1)
 
@@ -59,8 +60,8 @@ def meetup_js():
         """.format(**return_value())
 
 
-@app.route('/test.json')
+@app.route('/<meetup>.json')
 @cross_origin()
-def test_json():
+def test_json(meetup):
     """ Test CORS JSON """
-    return jsonify(return_value())
+    return jsonify(return_value(meetup))
